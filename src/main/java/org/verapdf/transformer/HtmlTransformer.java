f93605707e8b4f3df1ccfd4fd1a1791aeec6ff46
Evgeniy Muravitskiy <evgeniy.muravitskiy@duallab.com>
2015-09-03 14:23:40 +0300

    Fixes in html transformer for resource upload

--- src/main/java/org/verapdf/transformer/HtmlTransformer.java
+++ src/main/java/org/verapdf/transformer/HtmlTransformer.java
@@ -9,13 +9,7 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.*;
 
 /**
  * @author Evgeniy Muravitskiy
@@ -24,22 +18,21 @@ public class HtmlTransformer {
 
 	private static final Logger LOGGER = Logger.getLogger(HtmlTransformer.class);
 
-	private static final String XSLT_SCHEMA = getSystemIndependentPath("/htmlPattern.xsl");
+	private static final String XSLT_SCHEMA_NAME = "/htmlPattern.xsl";
+	private static final File XSLT_SCHEMA = getSystemIndependentFile();
 
-	public static final int FILE_SYSTEM_CONST = 1;
+	private HtmlTransformer() {
+		// disable default constructor
+	}
 
 	public static void transformToHtml(VeraPDFTransformConfig config) {
 		File file = new File(config.getInputPath());
 		if (file.isFile() && file.getName().endsWith(".xml")) {
-			try {
-				System.out.println(XSLT_SCHEMA);
+			try (OutputStream htmlFile = new FileOutputStream(config.getOutputPath())) {
 				TransformerFactory tFactory = TransformerFactory.newInstance();
 
-				File schema = new File(XSLT_SCHEMA);
-				Source xslDoc = new StreamSource(schema);
+				Source xslDoc = new StreamSource(XSLT_SCHEMA);
 				Source xmlDoc = new StreamSource(file);
-
-				OutputStream htmlFile = new FileOutputStream(config.getOutputPath());
 
 				Transformer transformer = tFactory.newTransformer(xslDoc);
 
@@ -51,25 +44,34 @@ public class HtmlTransformer {
 				transformer.transform(xmlDoc, new StreamResult(htmlFile));
 			} catch (Exception e) {
 				LOGGER.error("Problems with transforming to html.", e);
+			} finally {
+				if (XSLT_SCHEMA != null) {
+					XSLT_SCHEMA.delete();
+					XSLT_SCHEMA.deleteOnExit();
+				}
 			}
 		} else {
 			LOGGER.error("File not found or this is not xml.");
 		}
 	}
 
-	private static String getSystemIndependentPath(String path) {
-		try {
-			URL resourceUrl = ClassLoader.class.getResource(path);
-			/*URL url = ClassLoader.class.getResource("");
-			Map<String, String> env = new HashMap<>(FILE_SYSTEM_CONST);
-			env.put("create", "true");
-			FileSystem zipfs = FileSystems.newFileSystem(resourceUrl.toURI(), env);*/
-			Path resourcePath = Paths.get(resourceUrl.toURI());
-			return resourcePath.toString();
-		} catch (URISyntaxException e) {
-			LOGGER.error("Can`t get path to resources.", e);
+	private static File getSystemIndependentFile() {
+		try (InputStream resourceUrl = ClassLoader.class.getResourceAsStream(XSLT_SCHEMA_NAME)) {
+			File file = new File(XSLT_SCHEMA_NAME);
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			OutputStream writer = new BufferedOutputStream(new FileOutputStream(file));
+			byte[] bytes = new byte[resourceUrl.available()];
+			resourceUrl.read(bytes);
+			writer.write(bytes);
+			writer.close();
+			return file;
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
 		return null;
 	}
+
 }
